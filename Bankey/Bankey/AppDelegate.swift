00ba360f706b3a54cde625a5bb274fed01b4105a
fmarsico <Fdossantosmarsi3741@conestogac.on.ca>
2024-05-16 15:28:19 -0400

    First Commit

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Francesco Marsico on 2024-05-12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+//        window?.rootViewController = LoginViewController()
+//        window?.rootViewController = OnboardingContainerViewController()
+        
+        return true
+    }
+    
+
+
+}
+
